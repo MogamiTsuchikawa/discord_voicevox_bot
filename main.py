2f3170ca6613d359ba04048578da439d7cd0f5b2
Chroma7p <nahco3.hcl@gmail.com>
2023-05-27 10:30:29 +0900

    コメントの修正

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 # load_dotenv()
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-# botのオブジェクトを作成(コマンドのトリガーを!に)
-bot = commands.Bot(
+# botのオブジェクトを作成
+bot: commands.Bot = commands.Bot(
     command_prefix="/",
     intents=discord.Intents.all(),
     application_id=os.getenv("APPLICATION_ID"),
-    # activity=discord.Activity(name="準備中")
 )
 
 
@@ -27,11 +26,11 @@ async def main():
     cogfolder = "cogs."
     cogs = ["main_cog"]
     for cog in cogs:
-        await bot.load_extension(cogfolder+cog)
+        await bot.load_extension(cogfolder + cog)
     await bot.tree_sync()
 
     async with bot:
-        # Botのトークンを指定（デベロッパーサイトで確認可能）
+        # Botのトークンを指定（Developer Portalで確認可能）
         await bot.start(os.getenv("DISCORD_BOT_TOKEN"))
 
 asyncio.run(main())
